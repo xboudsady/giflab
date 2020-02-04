4e63444e0f04afbaf6d98d7065febf08e74ca504
xboudsady <boudsadyx@gmail.com>
2020-02-03 20:22:03 -0800

    Initial Commit

---
+++ src/main/java/com/teamtreehouse/giflib/AppConfig.java
@@ -0,0 +1,11 @@
+package com.teamtreehouse.giflib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class AppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
+}
