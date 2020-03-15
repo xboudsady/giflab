ccd131b94fc1e502f76f1011a3209074e128b89b
xboudsady <boudsadyx@gmail.com>
2020-03-15 13:04:40 -0700

    feature: Added dynamic url path, and list data for retrieval

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,8 +1,11 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.data.GifRepository;
 import com.teamtreehouse.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,16 +13,20 @@ import java.time.LocalDate;
 
 @Controller
 public class GifController {
+    @Autowired
+    private GifRepository gifRepository;
 
     @RequestMapping("/")               // Handles request to our main or home page
     public String listGifs() {
         return "home";                 // Returns page from main/resources/templates/home.html
     }
 
-    @RequestMapping("/gif")
-    public String gifDetails(ModelMap modelMap){
+
+    // URL => http://localhost:8080/gif/android-explosion
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap){
         // Construct a new Gif Object so our view has access
-        Gif gif = new Gif("compiler-bot", LocalDate.of(2020,3,15), "Seth Boudsady", true);
+        Gif gif = gifRepository.findByName(name);
         // Add to modelMap
         modelMap.put("gif", gif);
         return "gif-details";
