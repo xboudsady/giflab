46a86d37bfd7182f761f99d062fe92759d010fbb
xboudsady <boudsadyx@gmail.com>
2020-03-15 11:28:58 -0700

    feature: Created new Gif.java model, updated GifController

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,8 +1,12 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
@@ -12,6 +16,13 @@ public class GifController {
         return "home";                 // Returns page from main/resources/templates/home.html
     }
 
-
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        // Construct a new Gif Object so our view has access
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2020,3,15), "Seth Boudsady", true);
+        // Add to modelMap
+        modelMap.put("gif", gif);
+        return "gif-details";
+    }
 
 }
--- src/main/java/com/teamtreehouse/giflib/model/Gif.java
+++ src/main/java/com/teamtreehouse/giflib/model/Gif.java
@@ -1,18 +1,22 @@
 package com.teamtreehouse.giflib.model;
 
+import java.time.LocalDate;
+
 public class Gif {
     private String name;
-    private LocatDate dateUploaded;
+    private LocalDate dateUploaded;
     private String username;
     private boolean favorite;
 
-    public Gif(String name, LocatDate dateUploaded, String username, boolean favorite) {
+    // Constructor
+    public Gif(String name, LocalDate dateUploaded, String username, boolean favorite) {
         this.name = name;
         this.dateUploaded = dateUploaded;
-        this.usernam = usernam;
+        this.username = username;
         this.favorite = favorite;
     }
 
+    // Getter & Setter to access private class variables
     public String getName() {
         return name;
     }
@@ -21,20 +25,20 @@ public class Gif {
         this.name = name;
     }
 
-    public LocatDate getDateUploaded() {
+    public LocalDate getDateUploaded() {
         return dateUploaded;
     }
 
-    public void setDateUploaded(LocatDate dateUploaded) {
+    public void setDateUploaded(LocalDate dateUploaded) {
         this.dateUploaded = dateUploaded;
     }
 
     public String getUsername() {
-        return usernam;
+        return username;
     }
 
-    public void setUsername(String usernam) {
-        this.usernam = usernam;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public boolean isFavorite() {
