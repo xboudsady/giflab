9869c4150ef67e4fa8691fc696dab606183d2a3b
xboudsady <boudsadyx@gmail.com>
2020-03-15 13:48:54 -0700

    feature: Updated homepage

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,5 +1,6 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import com.teamtreehouse.giflib.data.GifRepository;
 import com.teamtreehouse.giflib.model.Gif;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,25 +11,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 public class GifController {
     @Autowired
     private GifRepository gifRepository;
 
-    @RequestMapping("/")               // Handles request to our main or home page
-    public String listGifs() {
-        return "home";                 // Returns page from main/resources/templates/home.html
+    @RequestMapping("/")                                    // Handles request to our main or home page
+    public String listGifs(ModelMap modelMap) {
+        List<Gif> allGifs = gifRepository.getAllGifs();     // Get all data from our data list
+        modelMap.put("gifs", allGifs);                      // Puts all the gif objects into our moelMap, so our thymeleaf have access
+        return "home";                                      // Returns page from main/resources/templates/home.html
     }
 
 
     // URL => http://localhost:8080/gif/android-explosion
     @RequestMapping("/gif/{name}")
     public String gifDetails(@PathVariable String name, ModelMap modelMap){
-        // Construct a new Gif Object so our view has access
-        Gif gif = gifRepository.findByName(name);
-        // Add to modelMap
-        modelMap.put("gif", gif);
+        Gif gif = gifRepository.findByName(name);      // Construct a new Gif Object so our view has access
+        modelMap.put("gif", gif);                      // Add to modelMap
         return "gif-details";
     }
 
